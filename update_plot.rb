d96da6ffc9d2c32d41934100007bb37b517445f7
Noah <noah@microglacoste.com>
2011-01-31 15:32:33 -0700

    Added code for the case of an empty hash file.

--- update_plot.rb
+++ update_plot.rb
@@ -9,33 +9,26 @@ md5_new = Digest::MD5.hexdigest(File.read(CONSTANTS["earthquake_file_path"]))
 gnuplot_script = GnuplotScript.new(CONSTANTS['gnuplot_script_path'], METERS)
 ftp_script = FtpScript.new(CONSTANTS['ftp_script_path'], CONSTANTS['www_ftp_user'], CONSTANTS['www_ftp_pass'], [CONSTANTS['plot_file_path']])
 
-if File.exist?(hash_file_path)
-  md5_f = File.new(hash_file_path,"r+")
-  md5_s = md5_f.gets.chomp
-  
-  if md5_s == md5_new
-    puts "No changes detected in '#{CONSTANTS['earthquake_file_path']}'"
-  else
-    puts "Creating gnuplot script..."
-    gnuplot_script.create
-    puts "Running '#{CONSTANTS["data_file_path"]}' to gnuplot..."
-    gnuplot_script.execute
-    
-    puts "\nCreating FTP script..."
-    ftp_script.create
-    puts "Running FTP script..."
-    ftp_script.execute
-    
-    puts "\nWriting new MD5: #{md5_new}"
-    md5_f.rewind
-    md5_f.print md5_new
-  end
+md5_f = File.new(hash_file_path,"a+")
+md5_f.rewind
+md5_s = md5_f.gets.chomp unless md5_f.eof?
+
+puts "#{md5_s} == #{md5_new}"
+if md5_s == md5_new
+  puts "No changes detected in '#{CONSTANTS['earthquake_file_path']}'"
 else
-  puts "Creating '#{hash_file_path}'"
-  md5_f = File.new(hash_file_path,'w')
+  puts "Creating gnuplot script..."
+  gnuplot_script.create
+  puts "Running '#{CONSTANTS["data_file_path"]}' to gnuplot..."
+  gnuplot_script.execute
   
-  puts "Writing MD5: #{md5_new}"
+  puts "\nCreating FTP script..."
+  ftp_script.create
+  puts "Running FTP script..."
+  ftp_script.execute
+  
+  puts "\nWriting new MD5: #{md5_new}"
+  md5_f.rewind
   md5_f.print md5_new
 end
-
 sleep 2
